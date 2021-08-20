00989b637573f488decb7bf069cbdff6c5257781
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-08-20 16:46:14 -0700

    validation-in-place

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -9,9 +9,7 @@ const commentController = {
         return Pizza.findOneAndUpdate(
           { _id: params.pizzaId },
           { $push: { comments: _id } },
-          { new: true },
           { new: true, runValidators: true }
-
         );
       })
       .then(dbPizzaData => {
@@ -53,7 +51,6 @@ removeComment({ params }, res) {
     Comment.findOneAndUpdate(
       { _id: params.commentId },
       { $push: { replies: body } },
-      { new: true },
       { new: true, runValidators: true }
 
     )
